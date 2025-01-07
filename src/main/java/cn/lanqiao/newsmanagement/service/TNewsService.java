3240e0ea755ac66c49bfba4e260c63469c7daf44
王晓芳 <859442473@qq.com>
2025-01-07 14:05:25 +0800

    分页

--- src/main/java/cn/lanqiao/newsmanagement/service/TNewsService.java
+++ src/main/java/cn/lanqiao/newsmanagement/service/TNewsService.java
@@ -1,6 +1,7 @@
 package cn.lanqiao.newsmanagement.service;
 
 import cn.lanqiao.newsmanagement.model.pojo.TNews;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -8,17 +9,18 @@ public interface TNewsService {
     //新闻列表
 //    List<TNews> selectAllNews();
     List<TNews> selectAllNews(Integer pageNum,Integer pageSize); //ct
+    List<TNews> selectAllNewsList(Integer pageNum, Integer pageSize);
     //按分类查询新闻
     List<TNews> selectNewsBySort(String sort);
     //ct
     //通过标题查询新闻
     List<TNews> selectNewsByTitle(String title);
     Integer selectNewsTotal();
-
   Integer updateAudit(String id);
 
   Integer updateAuditno(String id);
 
   Object updateRemarks(String id, String remarks);
   //ct
+
 }
